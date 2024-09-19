8248e891eb663239cbd88cc0b5a946d08621fdb3
Miles Contopoulos <107159084+TheMagnificentMiles@users.noreply.github.com>
2024-09-19 14:18:01 -0400

    Update control.ino

--- control/control.ino
+++ control/control.ino
@@ -1,11 +1,43 @@
 //https://projecthub.arduino.cc/tmekinyan/how-to-use-the-nrf24l01-module-with-arduino-813957
 
-void setup() {
-  // put your setup code here, to run once:
+#include "SPI.h" 
+#include "RF24.h" 
+#include "nRF24L01.h" 
+#define CE_PIN 9 
+#define CSN_PIN 10 
+#define INTERVAL_MS_TRANSMISSION 4000 
 
-}
+RF24 radio(CE_PIN, CSN_PIN); 
 
-void loop() {
-  // put your main code here, to run repeatedly:
+const byte address[6] = "00001";
 
-}
+//NRF24L01 buffer limit is 32 bytes (max struct size) 
+
+struct payload { 
+	 char[4] companyNumber; 
+	 int time;
+   int depth; 
+}; 
+
+payload payload; 
+
+void setup() 
+{ 
+	 Serial.begin(115200); 
+	 radio.begin(); 
+	 radio.setAutoAck(false);
+	 radio.setDataRate(RF24_1MBPS); 
+	 radio.setPALevel(RF24_PA_MAX);
+	 radio.setPayloadSize(sizeof(payload)); 
+	 radio.openWritingPipe(address); 
+	 radio.stopListening(); 
+} 
+
+void loop() 
+{ 
+	 payload.companyNumber = '1234'; 
+	 payload.time = 123;
+   payload.depth = 123;
+	 radio.write(&payload, sizeof(payload)); 
+	 delay(INTERVAL_MS_TRANSMISSION); 
+} 
