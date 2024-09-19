d2ac4f520bf85d8ebc10a87446f44a7848083e36
Miles Contopoulos <107159084+TheMagnificentMiles@users.noreply.github.com>
2024-09-19 14:09:44 -0400

    Setup

---
+++ control/control.ino
@@ -0,0 +1,11 @@
+//https://projecthub.arduino.cc/tmekinyan/how-to-use-the-nrf24l01-module-with-arduino-813957
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
